3f4d691bb31e03f81c98d6c8efdd0bc8055c9c81
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2023-09-07 11:05:22 -0500

    ✨ Local sitffness matrix generator

--- main.jl
+++ main.jl
@@ -31,3 +31,12 @@ for m = axes(element, 1)
         end
     end
 end
+
+function stimaB(coord)
+    N = coord[:] * ones(1, 3) - repmat(coord, 3, 1)
+    C = diag([norm(N[5,6], 2), norm(N[1, 2], 3), norm(N[1, 2], 2)])
+    M = spdiags([ones(6,1), ones(6,1), 2 * ones(6,1), ones(6,1)], [-4,-2,0, 2, 4], 6, 6)
+    C * N' * M * N * C / (24 * det([1, 1, 1; coord]))
+end 
+
+
