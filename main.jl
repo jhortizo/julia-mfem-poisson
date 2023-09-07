e19614e93152b5031d6ba5cdf72f2b7754bde621
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2023-09-07 10:03:21 -0500

    ✨ Add nodes2edges calculation

--- main.jl
+++ main.jl
@@ -13,3 +13,9 @@ for j = axes(elements, 1)
     nodes2element[elements[j, :], elements[j, [2, 3, 1]]] .+= j .* Matrix(1I, 3, 3)
 end
 
+B = nodes2element + nodes2element';
+indices = findall(!iszero, triu(B))
+rows, cols = [i[1] for i in indices], [i[2] for i in indices]
+nodes2edge = sparse(rows, cols, 1:size(rows, 1), size(coords, 1), size(coords, 1))
+nodes2edge = nodes2edge + nodes2edge'
+
