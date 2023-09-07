bf9009134730a2c2b68c355a2da7df3b57fd835f
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2023-09-07 10:42:15 -0500

    ✨ Add edge2element calculation

--- main.jl
+++ main.jl
@@ -5,12 +5,12 @@ using SparseArrays
 filepath = "data/"
 
 coords = readdlm(filepath * "coordinates.dat", ' ', Float64, '\n');
-elements = readdlm(filepath * "element.dat", ' ', Int, '\n');
+element = readdlm(filepath * "element.dat", ' ', Int, '\n');
 dirichlet = readdlm(filepath * "dirichlet.dat", ' ', Int, '\n');
 
 nodes2element = spzeros(size(coords, 1), size(coords, 1))
-for j = axes(elements, 1)
-    nodes2element[elements[j, :], elements[j, [2, 3, 1]]] .+= j .* Matrix(1I, 3, 3)
+for j = axes(element, 1)
+    nodes2element[element[j, :], element[j, [2, 3, 1]]] .+= j .* Matrix(1I, 3, 3)
 end
 
 B = nodes2element + nodes2element';
@@ -19,3 +19,15 @@ rows, cols = [i[1] for i in indices], [i[2] for i in indices]
 nodes2edge = sparse(rows, cols, 1:size(rows, 1), size(coords, 1), size(coords, 1))
 nodes2edge = nodes2edge + nodes2edge'
 
+noedges = size(rows, 1)
+edge2element = zeros(noedges, 4)
+for m = axes(element, 1)
+    for k = 1:3
+        initial_node = element[m, k]
+        end_node = element[m, k%3+1]
+        p = nodes2edge[initial_node, end_node]
+        if edge2element[p, 1] == 0
+            edge2element[p, :] = [initial_node end_node nodes2element[initial_node, end_note] nodes2element[end_node, initial_node]]
+        end
+    end
+end
