aeec31ea2ef32430b99c67999bb022ab389999d9
José Ortiz-Ocampo <joseortiz1807@gmail.com>
2023-10-22 13:58:42 -0500

    🐛 Fix dot operation

--- UserFunctions.jl
+++ UserFunctions.jl
@@ -1,4 +1,5 @@
 module UserFunctions
+using LinearAlgebra
 
 export fcn_zeros, fcn_ones, g_91
 
@@ -13,7 +14,7 @@ end
 function g_91(x, n)
     a = angle.((x[:, 1] + x[:, 2] * im) * (-1 - im) / sqrt(2)) .+ pi * 3 / 4 
     r = sqrt.(x[:, 1] .^ 2 + x[:, 2] .^ 2)
-    (2 / 3 * r .^ (-1 / 3) .* [-sin.(a / 3), cos.(a / 3)]) .* n' # TODO: solve dot product 
+    dot((2 / 3 * r .^ (-1 / 3) .* [-sin.(a / 3) cos.(a / 3)]), n) # TODO: solve dot product 
 end
 
 end
